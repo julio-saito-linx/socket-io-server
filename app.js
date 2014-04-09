f3b5f521aafe579ed4e45bb8765be27b3a11a203
Julio Makdisse Saito <saitodisse@gmail.com>
2014-04-09 18:08:37 -0300

    nothing to do yet

--- app.js
+++ app.js
@@ -23,10 +23,3 @@ io.sockets.on('connection', function (socket) {
     console.log(data);
   });
 });
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
