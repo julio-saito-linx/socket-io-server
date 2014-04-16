db9532daa7f62dcbc59bad611cb111663064e3e8
Julio Makdisse Saito <saitodisse@gmail.com>
2014-04-16 12:08:57 -0300

    using express

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
 
-app.listen(9003);
+server.listen(9003);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
-    res.writeHead(200);
-    res.end(data);
-  });
-}
 
 io.sockets.on('connection', function (socket) {
-  
   socket.emit('yourAreConnected', new Date());
-  
   socket.on('my other event', function (data) {
     console.log(data);
   });
-
   socket.on('toSocket:playlist:add', function (data) {
     console.log("\n toSocket:playlist:add \n", data);
     socket.broadcast.emit('toAll:playlist:add', data);
   });
-
 });
