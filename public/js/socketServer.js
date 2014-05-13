9468c3529a7a47ca0ee79758766cadbf638848e2
Julio Makdisse Saito <saitodisse@gmail.com>
2014-05-13 13:30:45 -0300

    "$1"

--- public/js/socketServer.js
+++ public/js/socketServer.js
@@ -1,31 +1,47 @@
+/*
+++++++++++++++
+    client
+++++++++++++++
+ */
+
+/*global io*/
+'use strict';
+
 $(function() {
     var jPreLog = $('.log');
     var jUlUsers = $('.users');
 
     var log = function(eventName, data) {
         console.log(eventName, data);
-        jPreLog.append('<span class="eventName">' + eventName + ":</span>");
-        jPreLog.append('<span class="data">' + JSON.stringify(data, ' ', 2) + "</span>");
-        jPreLog.append('<br />');
+        // jPreLog.append('<span class="eventName">' + eventName + ':</span>');
+        // jPreLog.append('<span class="data">' + JSON.stringify(data, ' ', 2) + '</span>');
+        // jPreLog.append('<br />');
     };
 
-    var renderSocketClients = function(io_sockets_clients_count) {
-        jUlUsers.html('Total users connected: ' + io_sockets_clients_count);
+    var renderTotalConnections = function(data) {
+        jUlUsers.html('Connections to socket.io: ' + data.usersCount);
     };
 
     // TODO: 192.168... must be dynamic
     var socket = io.connect('http://192.168.15.103');
     
     // connect or disconnect, always update
-    socket.on('clientsChanged', function (io_sockets_clients_count) {
-        renderSocketClients(io_sockets_clients_count);
+    socket.on('clientsChanged', function (data) {
+        renderTotalConnections(data);
+        log('clientsChanged', data);
     });
-    socket.on('userDisconnected', function (io_sockets_clients_count) {
-        renderSocketClients(io_sockets_clients_count);
+    socket.on('userDisconnected', function (data) {
+        renderTotalConnections(data);
+        log('userDisconnected',{});
+    });
+
+    socket.on('client:connection', function (data) {
+        log('client:connection', data);
     });
 
     socket.on('toAll:playlist:add', function (songJson) {
         log('toAll:playlist:add', '[' + songJson.id + '] ' + songJson.artist + ' - ' + songJson.title);
     });
+
 
 });
--- sockets/mainSocket.js
+++ sockets/mainSocket.js
@@ -1,18 +1,66 @@
+/*
+++++++++++++++
+    SERVER
+++++++++++++++
+ */
+
+'use strict';
+
+var serverData = {};
+(function() {
+  serverData.dateCreation = (new Date());
+})();
+
 exports.startSockets = function(io, userName){
-  'use strict';
+  serverData.userName = userName;
   io.sockets.on('connection', function (socket) {
 
     // connection
-    io.sockets.emit('clientsChanged', io.sockets.clients().length);
+    io.sockets.emit(
+      'clientsChanged', 
+      {
+        serverData: serverData,
+        usersCount: io.sockets.clients().length
+      }
+    );
     
+    socket.on('client:connection', function (data) {
+        console.log('client:connection received');
+        socket.broadcast.emit(
+          'client:connection', 
+          {
+            serverData: serverData,
+            clientData: data
+          }
+        );
+        
+        //send info from server
+        socket.emit('server:status', 
+        {
+          serverData: serverData
+        });
+    });
+   
     // on disconnection
     socket.on('disconnect', function () {
-        socket.broadcast.emit('clientsChanged', io.sockets.clients().length - 1);
+        socket.broadcast.emit('clientsChanged', 
+          {
+            serverData: serverData,
+            clientData: {
+              usersCount: io.sockets.clients().length - 1
+            }
+          }
+        );
     });
    
     socket.on('toSocket:playlist:add', function (data) {
       console.log('\n' + userName + ': toSocket:playlist:add \n', data);
-      socket.broadcast.emit('toAll:playlist:add', data);
+      socket.broadcast.emit('toAll:playlist:add', 
+        {
+          serverData: serverData,
+          data: data
+        }
+      );
     });
 
   });
